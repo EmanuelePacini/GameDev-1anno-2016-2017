a4f6f462924787825dcf700b2e192a9adf728008
Utente SoftDev1 <softdev1@comics.local>
2017-02-09 12:44:46 +0100

    UfoGame

---
+++ UFOgame/Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform playerTrsansform;
+	private Vector3 _offset;
+	// Use this for initialization
+	void Start () 
+	{
+		_offset = transform.position - playerTrsansform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = playerTrsansform.position + _offset;
+	}
+}
